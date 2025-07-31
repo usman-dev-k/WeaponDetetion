dbf9bbef101caa4c543218f3b85b456963fb7099
Usman Khalid <140155937+usman-dev-k@users.noreply.github.com>
2025-07-31 21:38:39 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 
 @st.cache_resource
 def load_model():
-    model_path = "yolov5s.pt"
+    model_path = "yolov8nn.pt"
     try:
         model = YOLO(model_path)
         st.success("✅ Model loaded successfully!")
